c5a9f8adc47eba6016c7394cbe4c3f26459ed4bc
WoolyMamooth <kispeterzsm@gmail.com>
2025-02-20 12:25:43 +0100

    Add text formatting utility functions to utils.py

--- dcalerts/utils.py
+++ dcalerts/utils.py
@@ -8,3 +8,87 @@ def create_timer(seconds_from_now):
     }
     """
     return "<t:"+str(int(time.time()+seconds_from_now))+":R>"
+
+def code_block(text:str):
+    """
+    Wraps text in a code block.
+    """
+    return "```"+text+"```"
+
+def bold(text:str):
+    """
+    Makes text bold.
+    """
+    return "**"+text+"**"
+
+def italic(text:str):
+    """
+    Makes text italic.
+    """
+    return "*"+text+"*"
+
+def underline(text:str):
+    """
+    Underlines text.
+    """
+    return "__"+text+"__"
+
+def strikethrough(text:str):
+    """
+    Strikes through text.
+    """
+    return "~~"+text+"~~"
+
+def spoiler(text:str):
+    """
+    Makes text a spoiler.
+    """
+    return "||"+text+"||"
+
+def quote(text:str):
+    """
+    Quotes text.
+    """
+    return "> "+text
+
+def block_quote(text:str):
+    """
+    Quotes text in a block.
+    """
+    return ">>> "+text
+
+def link(text:str, url:str):
+    """
+    Creates a hyperlink.
+    """
+    return "["+text+"]("+url+")"
+
+def mention(user_id:str):
+    """
+    Mentions a user.
+    """
+    return "<@"+user_id+">"
+
+def channel_mention(channel_id:str):
+    """
+    Mentions a channel.
+    """
+    return "<#"+channel_id+">"
+
+def role_mention(role_id:str):
+    """
+    Mentions a role.
+    """
+    return "<@&"+role_id+">"
+
+def emoji(emoji_id:str):
+    """
+    Adds an emoji.
+    """
+    return "<:"+emoji_id+">"
+
+def custom_emoji(emoji_id:str, emoji_name:str):
+    """
+    Adds a custom emoji.
+    """
+    return "<:"+emoji_name+":"+emoji_id+">"
